01962fc6bba9bd56a5516d9fa293d6d0b3512da6
Ian Dunn <itd3516@rit.edu>
2025-06-08 20:55:39 -0400

    Update ping

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,10 +1,11 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, MessageFlags } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder() // Command definition
         .setName('ping')
         .setDescription('pong'),
     async execute(interaction) { // Command functionality
-        await interaction.reply(':ping_pong: Pong!');
+        const sent = await interaction.reply({ content: ':ping_pong: Ping...', flags: MessageFlags.Ephemeral })
+        interaction.editReply(`:ping_pong: Pong!\nRoundtrip latency: ${sent.createdTimestamp - interaction.createdTimestamp}ms`);
     },
 };
