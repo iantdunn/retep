7c6b875178810d8da4bd161d9e998d4630363fbb
Ian Dunn <itd3516@rit.edu>
2025-06-20 00:33:56 -0400

    Add activity

--- events/ready.js
+++ events/ready.js
@@ -9,5 +9,14 @@ module.exports = {
 
         // Initialize reaction manager (includes reaction roles)
         await ReactionManager.initialize(client);
+
+        client.user.setActivity('S12 E19', { type: 'WATCHING' });
+
+        // await client.user.setPresence({
+        //     activities: [{ name: 'S12 E19', type: 'WATCHING' }],
+        //     status: 'online'
+        // });
+
+        console.log('Set presence to "Watching S12 E19"');
     },
 };
