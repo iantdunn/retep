0a267164d2a308083bad0284a585742e473b8825
ian1dunn <iantdunn@gmail.com>
2025-09-20 12:45:58 -0400

    PM2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  apps : [{
+    name   : "retep",
+    script : "./index.js",
+    out_file: "./logs/out.log",
+    error_file: "./logs/error.log",
+    log_date_format: "YYYY-MM-DD HH:mm:ss Z"
+  }]
+}
